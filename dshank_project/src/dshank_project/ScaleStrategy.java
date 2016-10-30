63079457259edc34c760b1723f9eeb9767bb6093
david <david@davidsThinkpad>
2016-10-30 17:21:02 -0400

    Changed PositioinStrategy to ScaleStrategy

---
+++ dshank_project/src/dshank_project/ScaleStrategy.java
@@ -0,0 +1,12 @@
+package dshank_project;
+
+/**
+ * Strategy for converting latitude and longitude values into pixels and vice versa
+ * @author david
+ *
+ */
+public class ScaleStrategy {
+	
+	
+
+}
